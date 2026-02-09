b7178d0e62c07f9b86383081187ba743a8c757d3
Petro <honcharukpetro1007@gmail.com>
2026-02-09 17:05:09 +0100

    delete coment

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,8 +1,7 @@
 import { styles } from "@/src/style";
 import React from "react";
 import { Text, View } from "react-native";
-//new coment for git test
-//second coment for git
+
 const HeaderComponent = () => {
   return (
     <View style={styles.headerList}>
